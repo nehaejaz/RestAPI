ccfd73c4df91a52844943808649a4dbb83e65aca
nehaejaz <neha.ejaz@icloud.com>
2019-05-31 12:24:40 +0500

    productID routes created

--- api/routes/products.js
+++ api/routes/products.js
@@ -14,6 +14,36 @@ router.post('/',(req,res,next)=>{
     })
 })
 
+router.get('/:productId',(req,res,next)=>{
+    const id= req.params.productId
+    if(id === 'special'){
+        res.status(200).json({
+            message:"You discovered Special ID :D",
+            id
+        })
+    } else {
+        res.status(200).json({
+            message:"you passed an ID",
+            id
+        })
+    }
+})
+
+router.patch('/:productId',(req,res,next) => {
+    const id=req.params.productId
+    res.status(200).json({
+        message:'Updated products',
+        id
+    })
+})
+
+router.delete('/:productId',(req,res,next) => {
+    const id= req.params.productId
+    res.status(200).json({
+        message:"deleted product",
+        id
+    })
+})
 module.exports= router
 
 
