3cd2266ca6fde0d6142ae477edc9d88c9b505803
nehaejaz <neha.ejaz@icloud.com>
2019-05-31 13:02:25 +0500

    added error handling packages

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express(); //getiing the express function
+const morgan = require('morgan')
 
 const ProductRoutes = require('./api/routes/products')
+const OrderRoutes = require('./api/routes/orders')
 
+app.use(morgan('dev'))
 //works as middleware
+//Our Routes which are handling the responses  
 app.use('/products',ProductRoutes)
+app.use('/orders',OrderRoutes)
 
 module.exports = app;
