38e1fcc554dffc9f1a5a0f7a0a4e2db26d9d730e
nehaejaz <neha.ejaz@icloud.com>
2019-06-03 13:06:16 +0500

    created body parser

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,8 +8,13 @@ router.get('/',(req,res,post) => {
 })
 
 router.post('/',(req,res,post) => {
+    const order = {
+        productId:req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-        message:"Orders posted"
+        message:"Orders posted",
+        Order:order
     })
 })
 
