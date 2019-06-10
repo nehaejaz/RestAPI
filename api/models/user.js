75ce18e5543f05f20259b1a686fd462956895993
nehaejaz <neha.ejaz@icloud.com>
2019-06-10 12:26:27 +0500

    completed the signup routes

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
 const userSchema = mongoose.Schema({
-    email:{type:String, required:true},
+    email:{type:String,
+        required:true,
+        unique:true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    },
     password:{type:String, require: true}
 });
 
